526773397368474b2a4f8d1b9d2bdadccc918b59
Lorenzo Fumi <lorenzofumi@gmail.com>
2023-12-13 19:05:24 +0100

    Hello World for swagger and RESTX

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,36 @@
-from flask import Flask, Blueprint, render_template, request, redirect, url_for, flash
+from flask import Flask, Blueprint, jsonify
+# from flask_swagger import swagger
+from flask_swagger_ui import get_swaggerui_blueprint
+
+from flask_cors import CORS
+
+from flask_restx import Api, Resource
+
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
 app = Flask(__name__)
+api = Api(app, doc='/api/docs')
 
-@app.route('/')
-def home():
-    return "Hello World!"
+@api.route('/api/')
+@api.doc(params={'id': 'An ID'}, description='Get a book by its ID')
+class Home(Resource):
+    def get(self):
+        return {'Hello': 'World'}
+
+# Swagger UI route
+SWAGGER_URL = '/api/docs'
+API_URL = '/swagger.json'
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "My API"
+    }
+)
+app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
 
 if __name__ == '__main__':
     # app.run(debug=True, host=os.environ.get('HOST'), port=os.environ.get('PORT'))
