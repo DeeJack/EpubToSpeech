d68f5a0d067e787ea26535bd35679856a461340f
Lorenzo Fumi <lorenzofumi@gmail.com>
2023-12-14 16:28:12 +0100

    Test for external IP

--- backend/tests/api/test_logs.py
+++ backend/tests/api/test_logs.py
@@ -16,3 +16,24 @@ def test_database_log(client):
         'query': 'Test database query'
     })
     assert response.status_code == 200
+    
+def test_external_api_log_bad_ip(client):
+    client.environ_base['REMOTE_ADDR'] = '192.1.1.1'  # Set the IP address
+    response = client.post('/api/log/external_api', json={
+        'message': 'Test external API message'
+    })
+    assert response.status_code == 403
+    
+def test_database_log_bad_ip(client):
+    client.environ_base['REMOTE_ADDR'] = '192.1.1.1'  # Set the IP address
+    response = client.post('/api/log/database', json={
+        'message': 'Test external API message'
+    })
+    assert response.status_code == 403
+    
+def test_error_log_bad_ip(client):
+    client.environ_base['REMOTE_ADDR'] = '192.1.1.1'  # Set the IP address
+    response = client.post('/api/log/error', json={
+        'message': 'Test external API message'
+    })
+    assert response.status_code == 403
