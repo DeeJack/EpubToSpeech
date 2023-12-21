640546ad937ef1efe65a06adc179873b9cf83319
Lorenzo Fumi <lorenzofumi@gmail.com>
2023-12-21 18:03:18 +0100

    Added appbar

--- frontend/src/layouts/default/Default.vue
+++ frontend/src/layouts/default/Default.vue
@@ -1,11 +1,19 @@
 <template>
   <v-app>
     <!-- <default-bar /> -->
+    <v-app-bar app>
+      <v-toolbar-title>EpubToSpeech</v-toolbar-title>
+      <v-spacer></v-spacer>
+
+      <v-btn to="/">Upload</v-btn>
+      <v-btn :to="'/tts?id=' + this.$route.query.id" v-if="this.$route.query.id">TTS</v-btn>
+      <v-btn :to="'/reader?id=' + this.$route.query.id" v-if="this.$route.query.id">Reader</v-btn>
+    </v-app-bar>
 
     <default-view />
   </v-app>
 </template>
 
 <script setup>
-  import DefaultView from './View.vue'
+import DefaultView from './View.vue'
 </script>
